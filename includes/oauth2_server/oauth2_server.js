f1a646c9b9110f8dcb56dfe620dc1c63cb4ca815
Tse Ho Yu Emilio <hytse6c@pantarei-design.com>
2012-08-17 10:32:59 +0800

    * snapshot

--- includes/oauth2_server/oauth2_server.js
+++ includes/oauth2_server/oauth2_server.js
@@ -7,22 +7,33 @@ function getParameterByName(name) {
     return decodeURIComponent(match[1].replace(/\+/g, ' '));
 }
 
-jQuery(document).ready(function () {
+jQuery(document).ready(function ($) {
   //if inside iframe
   if (window != parent && getParameterByName('code') != false) {
     parent.location.reload();
     return;
   }
-  //
-  //http://server7.oauth2test.pantarei-design.com/oauth2/authorize?response_type=code&client_id=9eb6db0c37e2067cbaa4b9dacf1c3922&redirect_uri=http://dev.client7.oauth2.pantarei-design.com
-  //http://server7.oauth2test.pantarei-design.com/oauth2/authorize?response_type=code&client_id=2a0d4c44690fbae5c94871686fb9408d&redirect_uri=http://dev.client7.oauth2.pantarei-design.com/
-  //console.debug(Drupal.settings.oauth2.url);
-  if (!Drupal.settings.oauth2.logged_in) {
+
+  
   var ifrm = document.createElement("IFRAME");
-  ifrm.setAttribute("src", Drupal.settings.oauth2.url+Drupal.settings.oauth2.authorize_uri+'?response_type=code&client_id='+Drupal.settings.oauth2.client_id+'&redirect_uri='+Drupal.settings.oauth2.redirect_uri);
   ifrm.style.width = 640+"px";
   ifrm.style.height = 480+"px";
-  //ifrm.style.display = 'none';
+  ifrm.style.display = 'none';
+  ifrm.id = 'oauth2_iframe';
+  
   document.body.appendChild(ifrm);
+  //console.debug(Drupal.settings.oauth2.url);
+  if (!Drupal.settings.oauth2.logged_in && Drupal.settings.oauth2.auto_login_enabled) {
+     $("#oauth2_iframe").attr('src',Drupal.settings.oauth2.url+Drupal.settings.oauth2.authorize_uri+'?response_type=code&client_id='+Drupal.settings.oauth2.client_id+'&redirect_uri='+Drupal.settings.oauth2.redirect_uri);
   }
+  
+  $('#oauth2_login_iframe').click(function() {
+
+     var iframe =  $("#oauth2_iframe").detach();
+       $(this).parent().append(iframe);
+       if (!$(iframe).attr('src')){
+           $(iframe).attr('src',Drupal.settings.oauth2.url+Drupal.settings.oauth2.authorize_uri+'?response_type=code&client_id='+Drupal.settings.oauth2.client_id+'&redirect_uri='+Drupal.settings.oauth2.redirect_uri);
+       }
+       $(iframe).show();
+  });
 });
