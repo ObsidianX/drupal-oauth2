9db281fe157ce663384bd597ee0e0569699ea292
Tse Ho Yu Emilio <hytse6c@pantarei-design.com>
2012-09-13 10:15:55 +0800

    update

--- includes/oauth2_server/oauth2_server.js
+++ includes/oauth2_server/oauth2_server.js
@@ -14,19 +14,19 @@ jQuery(document).ready(function ($) {
     return;
   }
 
-  
+
   var ifrm = document.createElement("IFRAME");
   ifrm.style.width = 640+"px";
   ifrm.style.height = 480+"px";
   ifrm.style.display = 'none';
   ifrm.id = 'oauth2_iframe';
-  
+
   document.body.appendChild(ifrm);
   //console.debug(Drupal.settings.oauth2.url);
   if (!Drupal.settings.oauth2.logged_in && Drupal.settings.oauth2.auto_login_enabled) {
      $("#oauth2_iframe").attr('src',Drupal.settings.oauth2.url+Drupal.settings.oauth2.authorize_uri+'?response_type=code&client_id='+Drupal.settings.oauth2.client_id+'&redirect_uri='+Drupal.settings.oauth2.redirect_uri);
   }
-  
+
   $('#oauth2_login_iframe').click(function() {
 
      var iframe =  $("#oauth2_iframe").detach();
